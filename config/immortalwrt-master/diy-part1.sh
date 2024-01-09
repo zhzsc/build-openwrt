e6c4769d9a26d38b82c79d7fc11e5bb76912d014
zhzsc <131594775+zhzsc@users.noreply.github.com>
2024-01-09 19:52:25 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -8,6 +8,9 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '$a src-git amlogic https://github.com/ophub/luci-app-amlogic  ' feeds.conf.default
+# sed -i '$a src-git argonnew https://github.com/jerrykuku/luci-theme-argon.git;main  ' feeds.conf.default
+# sed -i '$a src-git store https://github.com/linkease/istore.git;main  ' feeds.conf.default
 
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}
